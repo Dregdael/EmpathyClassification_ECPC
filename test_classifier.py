ea01fca3458af6a3e2df571277eb20f53331431f
Haru <edwincmv@hotmail.com>
2024-03-26 14:27:58 +0900

    PBC4cip functionality with CEM

---
+++ test_classifier.py
@@ -0,0 +1,100 @@
+import pickle
+import pandas as pd
+import torch
+import os
+
+import CEM as cem
+
+from PBC4cip import PBC4cip
+import os
+import argparse
+import numpy as np
+import pandas as pd
+
+from tqdm import tqdm, trange
+from PBC4cip import PBC4cip
+from PBC4cip.core.Evaluation import obtainAUCMulticlass
+from PBC4cip.core.Helpers import get_col_dist, get_idx_val
+
+def score(predicted, y):
+        y_class_dist = get_col_dist(y[f'{y.columns[0]}'])
+        real = list(map(lambda instance: get_idx_val(y_class_dist, instance), y[f'{y.columns[0]}']))
+        numClasses = len(y_class_dist)
+        confusion = [[0]* numClasses for i in range(numClasses)]
+        classified_as = 0
+        error_count = 0
+
+        for i in range(len(real)):
+            if real[i] != predicted[i]:
+                error_count = error_count + 1
+            confusion[real[i]][predicted[i]] = confusion[real[i]][predicted[i]] + 1
+
+        acc = 100.0 * (len(real) - error_count) / len(real)
+        auc = obtainAUCMulticlass(confusion, numClasses)
+        return confusion, acc, auc
+
+
+
+
+
+def main():
+
+    #setup of directories 
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    database_dir_ec = '/EmpatheticConversationsExchangeFormat/'
+    database_dir_ex = '/EmpatheticExchanges/'
+    current_db = database_dir_ex
+
+    #get test file to compare
+    testFile = current_dir + current_db + 'test.csv'
+    data_test = pd.read_csv(testFile)
+    data_test["empathy"] = data_test["empathy"].astype('int')
+    data_test["empathy"] = data_test["empathy"].astype('string')
+
+    x_test = data_test.drop(columns=['empathy'])
+    y_test = data_test.copy()
+    y_test = y_test.drop(columns=x_test.columns)
+
+    #get model
+    filename = current_dir + current_db + 'trained_pbc4cip.sav'
+    pbc = pickle.load(open(filename, 'rb'))
+
+    #predict with model
+    y_pred = pbc.predict(x_test)
+
+    #get evaluation metric
+    confusion, acc, auc = score(y_pred, y_test)
+    ClosenessEvaluationMeasure = cem.get_cem(y_pred,y_test)
+    
+    #send out predictions
+    with open(current_dir + current_db +"predictions.txt", "w") as f:
+        for prediction in y_pred:
+            print(f"{prediction}",file=f)
+
+    #print metrics
+    print(f'Closeness Evaluation Measure: {cem.get_cem(y_pred,y_test):.2f}')
+    print(f"\nConfusion Matrix:")
+    print(f'row: true, column: predicted ')
+    for i in range(len(confusion[0])):
+        for j in range(len(confusion[0])):
+            print(f"{confusion[i][j]} ", end='')
+        print("")
+    print(f"\n\nacc: {acc} , auc: {auc}, cem: {ClosenessEvaluationMeasure}")
+    
+    #Send results to database 
+    with open(current_dir + current_db +"results.txt", "w") as f:
+        print(f"Confusion Matrix:", file=f)
+        print(f'row: true, column: predicted ', file=f)
+        for i in range(len(confusion[0])):
+            for j in range(len(confusion[0])):
+               print(f"{confusion[i][j]} ", end='', file=f)
+            print("")
+        print(f"\n\nacc: {acc} , auc: {auc}, cem: {ClosenessEvaluationMeasure}", file=f)
+    
+
+
+    
+if __name__ == '__main__':
+
+    main()
+
---
+++ train_classifier.py
@@ -0,0 +1,52 @@
+import pickle
+import pandas as pd
+import torch
+import os
+
+from PBC4cip import PBC4cip
+import os
+import argparse
+import numpy as np
+import pandas as pd
+
+from tqdm import tqdm, trange
+from PBC4cip import PBC4cip
+from PBC4cip.core.Evaluation import obtainAUCMulticlass
+from PBC4cip.core.Helpers import get_col_dist, get_idx_val
+
+
+
+def main():
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    #database_dir = '/EmpatheticConversationsExchangeFormat/'
+    database_dir = '/EmpatheticExchanges/'
+    trainFile = current_dir + database_dir + 'train.csv'
+
+
+    pbc = PBC4cip(tree_count = 100,filtering=False, multivariate = False) # create classifier with custom tree count
+ 
+ 
+    data_train = pd.read_csv(trainFile)
+    data_train["empathy"] = data_train["empathy"].astype('int')
+    data_train["empathy"] = data_train["empathy"].astype('string')
+    #data_train["speaker_emotion"] = data_train["speaker_emotion"].astype('category')
+    #data_train["listener_emotion"] = data_train["listener_emotion"].astype('category')
+
+    x_train = data_train.drop(columns=['empathy'])
+    #print(x_train)
+    y_train = data_train.copy()
+    y_train = y_train.drop(columns=x_train.columns)
+    #print(y_train)
+    
+    #training
+    patterns = pbc.fit(x_train,y_train)
+    filename = current_dir + database_dir + 'trained_pbc4cip.sav'
+    pickle.dump(pbc, open(filename, 'wb'))
+    with open(current_dir + database_dir +"patterns.txt", "w") as f:
+        for pattern in patterns:
+            print(f"{pattern}",file=f)
+
+if __name__ == '__main__':
+
+    main()
+
