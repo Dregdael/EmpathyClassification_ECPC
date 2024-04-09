d91a2de4b9bc4062afd3ce863aeaf9a4b8e4f019
Haru <edwincmv@hotmail.com>
2024-04-09 14:09:52 +0900

    added contration test

---
+++ contraction_test.py
@@ -0,0 +1,21 @@
+import pickle
+import pandas as pd
+import torch
+import os
+import contractions
+
+
+# contracted text
+text = '''Ill be there within 5 min. Shouldn't you be there too? 
+          I'd love to see u there my dear. It's awesome to meet new friends.
+          We've been waiting for this day for so long.'''
+ 
+# creating an empty list
+expanded_words = []    
+for word in text.split():
+  # using contractions.fix to expand the shortened words
+  expanded_words.append(contractions.fix(word))   
+   
+expanded_text = ' '.join(expanded_words)
+print('Original text: ' + text)
+print('Expanded_text: ' + expanded_text)
